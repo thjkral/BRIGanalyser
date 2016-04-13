21b5d3f93d39028b6d69d3c5805e66be10159446
KralTHJ <KralTHJ@13-890-0071.zkh.umcg.intra>
2016-04-13 14:13:33 +0200

    Firt commit

---
+++ src/mmb/tomkral/briganalyser/main/BRIGanalyser.java
@@ -0,0 +1,21 @@
+/*
+ * This code is created by:
+ * Tom Kral | Bioinformatics intern
+ * t.h.j.kral@umcg.nl | +31642935544
+ * 
+ * Commissioned by:
+ * University Medical Center Groningen | Medical Microbiology
+ */
+package mmb.tomkral.briganalyser.main;
+
+/**
+ *
+ * @author KralTHJ
+ */
+public class BRIGanalyser {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
