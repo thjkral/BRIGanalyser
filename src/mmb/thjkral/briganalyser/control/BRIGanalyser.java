9d8b0ebe2b6bbf9f67247f3e7c5f4765ace86952
KralTHJ <KralTHJ@13-890-0071.zkh.umcg.intra>
2016-05-03 16:56:38 +0200

    Ready to isolate UniqueMarkers

--- src/mmb/thjkral/briganalyser/control/BRIGanalyser.java
+++ src/mmb/thjkral/briganalyser/control/BRIGanalyser.java
@@ -55,7 +55,7 @@ public class BRIGanalyser {
         }
         
         FindUniqueMarkers fum = new FindUniqueMarkers();
-        
+        fum.start(ringList);
         
         
         
--- src/mmb/thjkral/briganalyser/model/FindUniqueMarkers.java
+++ src/mmb/thjkral/briganalyser/model/FindUniqueMarkers.java
@@ -25,10 +25,67 @@ public class FindUniqueMarkers {
         
         compare(ringList);
         
-    }
+    }//start()
 
     private void compare(ArrayList<Ring> ringList) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        
+        ArrayList<ArrayList<Gap>> overlappingGaps = new ArrayList<>();
+        
+        //compare gaps of first ring with the other gaps
+        for (int i = 0; i < ringList.get(0).getGapsArray().size(); i++) {//for every Gap in Ring 1
+            
+            Gap gapOne = (Gap) ringList.get(0).getGapsArray().get(i);
+            int start = gapOne.getStartCor();
+            int stop = gapOne.getStopCor();
+            
+            ArrayList<Gap> matches = new ArrayList<>();
+            matches.add(gapOne);
+            
+            for (int j = 0; j < ringList.size(); j++) {//for every other Ring
+                
+                for (int k = 0; k < ringList.get(j).getGapsArray().size(); k++) {//for every Gap of that Ring
+                    
+                    Gap gapTwo = (Gap) ringList.get(j).getGapsArray().get(k);
+                    int startTwo = gapTwo.getStartCor();
+                    int stopTwo = gapTwo.getStopCor();
+                    
+                    if (start >= startTwo && stop <= stopTwo) {
+                        matches.add(gapTwo);
+                    } else {
+                        continue;
+                    }
+                    
+                    
+                }
+            }
+            overlappingGaps.add(matches);
+        }
+        
+        
+        //isolate UMs
+        System.out.println("Number of overlappers: " + overlappingGaps.size());
+        overlappingGaps = isolateUniqueMarkers(overlappingGaps);
+        System.out.println("Number of valid overlappers: " + overlappingGaps.size());
+        System.out.println(overlappingGaps.get(0));
+        
+        
+        
+    }//compare()
     
-}
+    private ArrayList<ArrayList<Gap>> isolateUniqueMarkers (ArrayList<ArrayList<Gap>> overlappingGaps) {
+        
+        //is the length of each array the same as the number of rings?
+        //delete all who have length < number of rings
+        
+        for (int i = 0; i < overlappingGaps.size(); i++) {
+            ArrayList currList = (ArrayList) overlappingGaps.get(i);
+            if (currList.size() < 6) {
+                overlappingGaps.remove(i);
+            }
+        }
+        
+        return overlappingGaps;
+        
+    }//isolateUniqueMarkers()
+    
+}//class()
